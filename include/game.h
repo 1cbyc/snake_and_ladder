8e9b03135753fca27652cc4d6eef3c3b70ea0b16
1cbyc <isaacnsisong@gmail.com>
2024-12-30 00:54:20 +0100

    Changes made on 2024-12-30 00:54:20

---
+++ include/game.h
@@ -0,0 +1,22 @@
+#ifndef GAME_H
+#define GAME_H
+
+class SnakeAndLadderGame {
+public:
+    SnakeAndLadderGame();
+    void startGame();
+    void rollDice(int player);
+    void movePlayer(int player, int diceRoll);
+    bool isGameOver();
+    void displayBoard();
+    void announceWinner();
+    
+private:
+    int player1Pos;
+    int player2Pos;
+    int board[100];
+    void initializeBoard();
+    void displayPlayerPosition(int player);
+};
+
+#endif
